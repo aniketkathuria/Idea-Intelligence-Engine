3647af302c2a08b2457ffdf14b5316e98860dcef
Aniket <kathuriaaniket2017@gmail.com>
2026-04-04 18:21:49 +0530

    fixed logger in main

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,10 @@
 import logging
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+logger = logging.getLogger(__name__)
 import sys
 import os
 
